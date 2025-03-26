cf18e41eee935d62f30e17d2dcf436d383ec31a6
jozef-rzadkosz <jozefrzadkosz09@gmail.com>
2025-03-26 17:18:59 +0100

    Changed gallery component

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -4,8 +4,6 @@ import { galleryList } from 'consts';
 import { GalleryImage } from 'models';
 import { IAlbum, Lightbox, LightboxConfig, LightboxModule } from 'ngx-lightbox';
 
-// Using IAlbum directly instead of creating a custom interface
-
 @Component({
   selector: 'app-gallery',
   standalone: true,
@@ -19,36 +17,24 @@ export class GalleryComponent implements OnInit {
   private _images = signal<GalleryImage[]>(galleryList);
   protected readonly images: Signal<GalleryImage[]> = this._images.asReadonly();
 
-  // Array for lightbox items
   protected lightboxImages: IAlbum[] = [];
 
   constructor() {
     this.lightboxConfig.disableScrolling = true;
     this.lightboxConfig.centerVertically = true;
-    this.lightboxConfig.fitImageInViewPort = true;
     this.lightboxConfig.showImageNumberLabel = true;
-    this.lightboxConfig.wrapAround = true;
     this.lightboxConfig.albumLabel = `Zdjęcie %1 z %2`;
-    // this.lightboxConfig.positionFromTop = 50;
-    // this.lightboxConfig.alwaysShowNavOnTouchDevices = true;
-    // this.lightboxConfig.wrapAround = true;
   }
 
   ngOnInit(): void {
-    // Transform gallery images to lightbox format
     this.lightboxImages = this.images().map((image) => ({
       src: image.src,
       caption: image.alt,
-      thumb: image.src
+      thumb: image.thumbnail
     }));
   }
 
   openLightbox(index: number): void {
-    console.log('XXX', this.lightboxImages);
     this.lightbox.open(this.lightboxImages, index);
-  }
-
-  closeLightbox(): void {
-    this.lightbox.close();
   }
 }
